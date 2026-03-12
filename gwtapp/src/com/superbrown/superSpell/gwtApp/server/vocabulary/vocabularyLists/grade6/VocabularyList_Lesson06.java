0583d0c4e55476e7d8f6b3fa146831503c21d7fb
Mike Brown <c-michael.brown2@charter.com>
2026-03-12 17:58:28 -0600

    fixed vocabulary word misspellings

--- gwtapp/src/com/superbrown/superSpell/gwtApp/server/vocabulary/vocabularyLists/grade6/VocabularyList_Lesson06.java
+++ gwtapp/src/com/superbrown/superSpell/gwtApp/server/vocabulary/vocabularyLists/grade6/VocabularyList_Lesson06.java
@@ -16,9 +16,9 @@ public class VocabularyList_Lesson06 extends VocabularyList
         addVocabularyWordToInventory("|adjacent| adj. adjoining; close to");
         addVocabularyWordToInventory("|artisan| n. a worker with a special manual skill in trade or industry; and artist");
         addVocabularyWordToInventory("|attain| v. to gain by hard work; to achieve; to reach");
-        addVocabularyWordToInventory("|decreased| adj. dead; n. a dead person or persons");
+        addVocabularyWordToInventory("|deceased| adj. dead; n. a dead person or persons");
         addVocabularyWordToInventory("|fragile| adj. easily broken or damaged; delicate; frail");
-        addVocabularyWordToInventory("|glorify| v. to give flory to; to honor; to make something seem better than it actually is");
+        addVocabularyWordToInventory("|glorify| v. to give glory to; to honor; to make something seem better than it actually is");
         addVocabularyWordToInventory("|harmonious| adj. make up of parts that are orderly and pleasing; agreeable; showing agreement in attitudes, feelings, and so on");
         addVocabularyWordToInventory("|incomparable| ajd. not capable of being compared; incapable of being equaled; matchless");
         addVocabularyWordToInventory("|multicolored| adj. having many colors");
